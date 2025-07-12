3cb1feeb5a13596819024c0032c2545fb2cacbb2
Jovicy <johnvictordml@gmail.com>
2025-07-12 17:56:09 +0100

    made the header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,18 +26,18 @@ const Header = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black/60 z-10"></div>
 
       {/* Content */}
-      <div className="container relative z-20 flex flex-col gap-6 justify-center min-h-[500px] px-4">
+      <div className="container relative z-20 flex flex-col gap-6 justify-center min-h-[500px]">
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="pt-10"
+          className="pt-10 md:pt-14"
         >
-          <h1 className="text-3xl md:text-5xl lg:text-7xl font-extrabold leading-tight text-white">
+          <h1 className="text-[31.5px] md:text-5xl lg:text-7xl font-semibold lg:font-extrabold md:font-extrabold leading-tight text-white">
             Welcome to <br />
             <span className="text-primary">DigitalIsland Solutions</span>
             <br />
-            <span className="text-primary flex items-center gap-3 text-xl md:text-2xl lg:text-3xl mt-2">
+            <span className="text-primary flex items-center gap-3 text-2xl md:text-2xl lg:text-7xl mt-2">
               <FaArrowRightLong /> AI + SupTech + RegTech
             </span>
           </h1>
